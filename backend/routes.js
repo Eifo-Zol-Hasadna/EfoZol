a0590dd3568b078dc8b756ada4e58b859772f6b5
mbearz <mbearz@gmail.com>
2015-08-10 20:38:48 -0700

    added authentication
    added code to the routes file to validate all incoming requests

--- backend/routes.js
+++ backend/routes.js
@@ -1,7 +1,43 @@
-var express		 = require("express");
-var router       = express.Router();
+var express = require("express");
+var router = express.Router();
+var jwt = require('jsonwebtoken');
 
 router.post("/api/external/login", require("./api/external/auth/postLogin.js").do);
+// all routes below need to be authenticated 
+router.use(function (req, res, next) {
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+  // decode token
+  if (token) {
+    // verifies secret and checks exp
+    jwt.verify(token, GLOBAL.Settings.secret, function (err, decoded) {
+      if (err) 
+      {
+        return res.status(403).send({
+          success: false,
+          message: 'Failed to authenticate token.'
+        });
+      }
+      else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;
+        next();
+      }
+    });
+  }
+  else {
+    // if there is no token
+    // return an error
+    return res.status(403).send({
+      success: false,
+      message: 'No token provided.'
+    });
+
+  }
+});
+
+
+
 
 //GET
 router.get("/api/external/branches", require("./api/external/branches/getBranches.js").do);
